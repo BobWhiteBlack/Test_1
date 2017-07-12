a07bfb2c88b1bf1b4cc74ab64d137ea3ac108f73
Bob <Bob@ALEX>
2017-07-12 23:37:14 +0300

    Добавление панели swt

---
+++ AllCalck/src/Calck/panel.java
@@ -0,0 +1,43 @@
+package Calck;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class panel {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					panel window = new panel();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public panel() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
